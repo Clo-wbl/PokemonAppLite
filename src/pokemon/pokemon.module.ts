75e4276c85fb3279b348c59a2fa287739002fd71
ChloeBrice <86777029+ChloeBrice@users.noreply.github.com>
2024-02-07 13:55:58 +0100

    avant essai du split de routes entre celles des pokemon et celles de l'app

--- src/pokemon/pokemon.module.ts
+++ src/pokemon/pokemon.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PokemonRoutingModule } from './pokemon-routing.module';
+
 import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
 import { PokemonDetailsComponent } from './pokemon-details/pokemon-details.component';
 
@@ -16,8 +17,6 @@ import { PokemonDetailsComponent } from './pokemon-details/pokemon-details.compo
 })
 export class PokemonModule {
 
-  constructor() {
-    console.log('pokemon module')
-  }
+  constructor() { }
 
- }
+}
