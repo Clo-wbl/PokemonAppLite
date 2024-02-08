9ba2259f8d09dcda6f7907d8d1372136fbc02a40
ChloeBrice <86777029+ChloeBrice@users.noreply.github.com>
2024-02-08 08:46:47 +0100

    remove warnings about undefined

--- src/pokemon/pokemon.ts
+++ src/pokemon/pokemon.ts
@@ -1,9 +1,9 @@
 export class Pokemon {
-    id?: number;
-    hp?: number;
-    cp?: number;
-    name?: string;
-    picture?: string;
-    types?: Array<string>;
-    created?: Date;
+    id: number | undefined;
+    hp: number | undefined;
+    cp: number | undefined;
+    name: string | undefined;
+    picture: string | undefined;
+    types: Array<string> | undefined;
+    created: Date | undefined;
 }
