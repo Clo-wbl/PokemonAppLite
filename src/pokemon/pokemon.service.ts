dd9fdf1f922ab140e3f3277f4587aa5c9fd4aeca
ChloeBrice <86777029+ChloeBrice@users.noreply.github.com>
2024-02-09 15:37:01 +0100

    search is working ! missing condition in service if term is too short

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -23,10 +23,14 @@ export class PokemonService {
     )
   }
 
-  searchPokemonList(term: string): Observable<Pokemon[]>{
-    return this.http.get<Pokemon[]>(`api/pokemons/name=${term}`).pipe(
-      tap((response)=>this.log(response)),
-      catchError((error)=>this.handleError(error, []))
+  searchPokemonList(term: string): Observable<Pokemon[]> {
+    if(term.length <= 1) {
+      return of([]);
+    } 
+
+    return this.http.get<Pokemon[]>(`api/pokemons/?name=${term}`).pipe(
+      tap((response) => this.log(response)),
+      catchError((error) => this.handleError(error, []))
     );
   }
 
